2b551c20a8884c65c4e85e57db7ce7c54f22f2b2
Sanjin <sanjinr@kth.se>
2023-12-13 11:19:33 +0100

    Initial

---
+++ Assets/Scripts/RVOController.cs
@@ -0,0 +1,209 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+namespace RVO
+{
+    public class RVOController : MonoBehaviour
+    {
+        private GameObject[] unityObstacles;
+        private Vector3[] goals;
+        private GameObject[] RVOAgents;
+        public GameObject prefab;
+        private Seeker[] seekers;
+        private Path[] paths;
+        private int[] currentNodeInPath;
+        private int agentCount = 20; // Total number of agents
+
+        public void AddUnityObstacleToRVO(GameObject unityObstacle)
+        {
+            // Assuming the obstacle is aligned with the world axes and
+            // using localScale to determine the size
+            Vector3 size = unityObstacle.transform.localScale / 2.0f; // half size for offset
+            Vector3 center = unityObstacle.transform.position;
+
+            // Calculate the corners of the base of the cube
+            Vector3 corner1 = center + new Vector3(-size.x, 0, size.z);
+            Vector3 corner2 = center + new Vector3(-size.x, 0, -size.z);
+            Vector3 corner3 = center + new Vector3(size.x, 0, -size.z);
+            Vector3 corner4 = center + new Vector3(size.x, 0, size.z);
+
+            // Create the obstacle for RVO
+            IList<Vector2> rvoObstacle = new List<Vector2>
+            {
+                new Vector2(corner1.x, corner1.z),
+                new Vector2(corner2.x, corner2.z),
+                new Vector2(corner3.x, corner3.z),
+                new Vector2(corner4.x, corner4.z)
+            };
+
+            // Add the obstacle to the RVO simulator
+            Simulator.Instance.addObstacle(rvoObstacle);
+
+            // Process the obstacle to take effect in the simulation
+            Simulator.Instance.processObstacles();
+        }
+
+        RVO.Vector2 toRVOVector(Vector3 param)
+        {
+            return new RVO.Vector2(param.x, param.z);
+        }
+
+        Vector3 toUnityVector(RVO.Vector2 param)
+        {
+            return new Vector3(param.x(), 2.5f, param.y());
+        }
+
+        void UpdateSeekers(Vector3 start, Vector3 end, int i){
+            void OnPathComplete(Path p)
+            {
+                if (!p.error)
+                {
+                    paths[i] = p;
+                }
+                else
+                {
+                    Debug.LogError("Path error: " + p.errorLog);
+                }
+            }
+
+            seekers[i].StartPath(start, end, OnPathComplete);
+        }
+
+        void UpdateAgentGoal(int agentIndex, Vector3 start, Vector3 goal){
+            currentNodeInPath[agentIndex] = 0;
+            goals[agentIndex] = goal;
+
+            UpdateSeekers(start, goals[agentIndex], agentIndex);
+        }
+
+        public void SetTimeStep(float t){
+            Simulator.Instance.setTimeStep(t);
+        }
+
+        public void InitializeObstacles(){
+            unityObstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+
+            for (int i = 0; i < unityObstacles.Length; i++){
+                AddUnityObstacleToRVO(unityObstacles[i]);
+                Debug.Log(unityObstacles[i].name);
+                Debug.Log("Added");
+            }
+        }
+
+        public void InitializeAgents(){
+            Simulator.Instance.setAgentDefaults(15.0f, 10, 5.0f, 5.0f, 1.0f, 1.0f, new RVO.Vector2(0.0f, 0.0f));
+
+            RVOAgents = new GameObject[agentCount];
+            goals = new Vector3[agentCount];
+            seekers = new Seeker[agentCount];
+            paths = new Path[agentCount];
+            currentNodeInPath = new int[agentCount];
+
+            // Instantiate agents and set their goals
+            for (int i = 0; i < agentCount; i++)
+            {
+                Vector3 spawnPosition, goalPosition;
+                currentNodeInPath[i] = 0;
+
+                if (i < agentCount / 2)
+                {
+                    // First half of agents
+                    spawnPosition = new Vector3(-30 -i,1,7);
+                    goalPosition = new Vector3(30,1,-7);
+                }
+                else
+                {
+                    // Second half of agents
+                    spawnPosition = new Vector3(30+i,1,-7);
+                    goalPosition = new Vector3(-30,1,7);
+                }
+
+                GameObject go = GameObject.Instantiate(prefab, spawnPosition, Quaternion.identity) as GameObject;
+                if (i < agentCount/2){
+                    var agentRenderer = go.GetComponent<Renderer>();
+                    agentRenderer.material.SetColor("_Color", Color.green);
+                }
+                RVOAgents[i] = go;
+                go.transform.parent = transform;
+                goals[i] = goalPosition;
+                seekers[i] = RVOAgents[i].AddComponent<Seeker>();
+
+                UpdateSeekers(spawnPosition, goals[i], i);
+                
+                Simulator.Instance.addAgent(toRVOVector(spawnPosition));
+                
+            }
+        }
+
+        public void InitializeSimulation(){
+            Simulator.Instance.setTimeStep(0.0f);
+            InitializeObstacles();
+            InitializeAgents();
+        }
+
+        public void ClearSimulation(){
+            for (int i = 0; i < agentCount; i++){
+                Destroy(RVOAgents[i]);
+            }
+            Simulator.Instance.Clear();
+        }
+        void Start()
+        {
+
+            Debug.Log("Simulation script starting!");
+            InitializeSimulation();
+
+            if (Simulator.Instance == null)
+            {
+                Debug.LogError("Simulator.Instance is null");
+                return;
+            }
+            else
+            {
+                Debug.Log("Everything seems to be working as it should!");
+                Debug.Log($"This data -> {Simulator.Instance.getNumAgents()}");
+            }
+        }
+
+        void Update()
+        {
+            for (int i = 0; i < Simulator.Instance.getNumAgents();i++) {
+                if (paths[i] == null){
+                    continue;
+                }
+                if (currentNodeInPath[i] >= paths[i].vectorPath.Count){
+                    Vector3 goal = new Vector3(40,1,40);
+                    UpdateAgentGoal(i, RVOAgents[i].transform.position, goal);
+                    continue;
+                }
+
+                Vector3 currentWaypoint = paths[i].vectorPath[currentNodeInPath[i]];
+                Vector3 directionToWaypoint = (currentWaypoint - RVOAgents[i].transform.position).normalized;
+
+                // Vector2 goalVector = toRVOVector(goals[i]) - Simulator.Instance.getAgentPosition(i);
+
+                // Use the direction to the waypoint to set the preferred velocity
+                Vector2 goalVector = toRVOVector(directionToWaypoint);
+
+                // Check if close to the current waypoint and increment index
+                if (Vector3.Distance(RVOAgents[i].transform.position, currentWaypoint) < 5.0f)
+                {
+                    currentNodeInPath[i]++; // Increment the waypoint index for this agent
+                }
+
+                if (RVOMath.absSq(goalVector) > 1.0f)
+                {
+                    goalVector = RVOMath.normalize(goalVector);
+                }
+
+                Simulator.Instance.setAgentPrefVelocity(i, goalVector);
+                //Debug.Log($"Prefered: {Simulator.Instance.getAgentPrefVelocity(i)}");
+                RVOAgents[i].transform.localPosition = toUnityVector(Simulator.Instance.getAgentPosition(i)); 
+            }
+            Simulator.Instance.doStep();
+        }
+
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+
+    public RVO.RVOController rvo;
+    public Slider slider;
+    public TextMeshProUGUI sliderText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sliderValue = Mathf.Round(slider.value);
+        sliderText.text = sliderValue.ToString();
+        SetTimeStep(sliderValue);
+    }
+
+    public void SetTimeStep(float t){
+        float ts = t/100;
+        rvo.SetTimeStep(ts);
+    }
+
+    //Reloads the Simulation
+	public void Reload(){
+        rvo.ClearSimulation();
+        rvo.InitializeSimulation();
+	}
+}
